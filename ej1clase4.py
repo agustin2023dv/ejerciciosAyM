44c1a762397a7b2e261ff1834b700cedc56a5a9c
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2023-08-29 23:15:14 +0200

    Ejercicio 1  clase 4

---
+++ ej1clase4.py
@@ -0,0 +1,13 @@
+'''
+Calcular el promedio de una lista de números(no menos de 5
+elementos) usando un acumulador.
+'''
+
+lista = [1,2,3,4,5]
+
+acumulador = 0
+
+for i in lista:
+    acumulador += i
+
+print(acumulador)
