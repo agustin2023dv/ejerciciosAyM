8cf6fdbf3d7e551505ce4f21274e5ff73f378a8f
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2023-08-30 06:43:20 +0200

    Ejercicio 4 clase 4. Numpy

---
+++ ej4clase4.py
@@ -0,0 +1,18 @@
+'''
+Realizar un arreglo tridimensional con los sgtes requisitos:
+
+Primero sub-arreglo 2x2 con múltiplos de 4
+
+Segundo sub-arreglo 4x4 con múltiplos de 3
+
+Tercer sub-arreglo 3X3 con múltiplos e 2
+'''
+
+import numpy as np
+
+primero = np.array([[2,2],
+                   [8,12]])
+segundo= np.array([[3,3,6,12],[6,12,18,42],[24,36,6,24],[6,3,18,12]])
+
+tercero = np.array([[2,4,6],[8,10,20],[30,40,50]])
+
